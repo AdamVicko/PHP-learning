e12a03aaaed78366cdcea5af712e7b6452712d44
AdamVicko <vickovic2203@gmail.com>
2022-12-20 21:34:13 +0100

    Update

---
+++ SQL/Excersises/excersise.1.sql
@@ -0,0 +1,181 @@
+#  c:\xampp\mysql\bin\mysql -uroot < C:\Users\Adam\Desktop\Repositories\PHP-learning\SQL\Excersises\excersise.1.sql
+
+drop database if exists excersise;
+create database excersise;
+use excersise;
+
+create table sestra(
+    sifra int not null primary key auto_increment,
+    introvertno bit,
+    haljina varchar(31) not null,
+    maraka decimal(18,7),
+    hlace varchar(46) not null,
+    narukvica int
+);
+
+create table zena (
+    sifra int not null primary key auto_increment,
+    treciputa datetime,
+    hlace varchar(46),
+    kratkamajica varchar(31) not null,
+    jmbag char(11) not null,
+    bojaociju varchar(36) not null,
+    haljina varchar(31),
+    sestra int
+);
+
+create table muskarac(
+    sifra int not null primary key auto_increment,
+    bojaociju varchar(36),
+    hlace varchar(46),
+    modelnaocala varchar(43),
+    maraka decimal(14,5) not null,
+    zena int
+);
+
+create table mladic(
+    sifra int not null primary key auto_increment,
+    suknja varchar(39),
+    kuna decimal(16,8),
+    drugiputa datetime,
+    asocijalno bit,
+    ekstrovertno bit,
+    dukserica varchar(48),
+    muskarac int
+);
+
+create table sestra_svekar(
+    sifra int not null primary key auto_increment,
+    sestra int,
+    svekar int
+);
+
+create table svekar(
+    sifra int not null primary key auto_increment,
+    bojaociju varchar(40),
+    prstena int,
+    dukserica varchar(48),
+    lipa decimal (13,8),
+    eura decimal(12,7),
+    majica varchar(35)
+);
+
+create table punac(
+    sifra int not null primary key auto_increment,
+    ogrlica int,
+    gustoca decimal(14,9),
+    hlace varchar(41)
+);
+
+create table cura(
+    sifra int not null primary key auto_increment,
+    novcica decimal(16,5),
+    gustoca decimal(18,6),
+    lipa decimal(13,10),
+    ogrlica int,
+    bojakose varchar(38),
+    suknja varchar(36),
+    punac int
+);
+
+
+
+alter table zena add foreign key (sestra) references sestra(sifra);
+alter table muskarac add foreign key (zena) references zena(sifra);
+alter table sestra_svekar add foreign key (sestra) references sestra(sifra);
+alter table sestra_svekar add foreign key (svekar) references svekar(sifra);
+alter table mladic add foreign key (muskarac) references muskarac(sifra);
+alter table cura add foreign key (punac) references punac(sifra);
+
+
+
+insert into sestra(sifra,haljina,hlace)
+values
+(1,'Ne nosi','Nosi'),
+(2,'Ne nosi','Ne nosi'),
+(3,'Nosi samo duge haljine preko koljena','Nosi'),
+(4,'Ne nosi','Ne nosi jos'),
+(5,'Nosi','Nnosi');
+
+insert into zena(sifra,treciputa,hlace,kratkamajica,jmbag,bojaociju,haljina,sestra)
+values
+(null,'2001-04-24 23-14-09','Ne nosi','Jako kratka majca','rzen57d8493','Zute oci','Uska haljina iznad koljena',1),
+(null,'2001-04-24 05-47-48','Ne nosi puno','Jako kratka majca iznad brade','rzen57d8493','Zudddddddddte oci','Uska haljinaaaaaaaaaaaaaaa iznad koljena',2),
+(null,'2001-11-05 13-14-09','Ne nosi','Jako kraddddddddtka majca','rzen57d8493','Zute oddddddddci','Uska haljinadddddddddddd iznad koljena',3);
+
+insert into muskarac(sifra,bojaociju,hlace,modelnaocala,maraka,zena)
+values(null,'Plavuskasta','Jako velike hlacice','Jako male naocale model dermatitis','225,49',1),
+(null,'Tozu','Jako male hlacice','Jako male naocale model zumberackogorje','22,49',2),
+(null,'Bijela','Nenosi hlace','Jako male naocale model Tuzla','356,00',3);
+
+insert into mladic(sifra,suknja,kuna,ekstrovertno,dukserica,muskarac)
+values
+(null,'Ne nosi','15.20',1,'Uvijek nosi',1),
+(null,'Ne nosi kad nemora','15000.00',0,'Nosi kad pada kisa',2),
+(null,'Ne nosi','258.20',1,'Samo na potkusulju',3);
+
+insert into svekar(sifra,dukserica)
+values 
+(1,'aaaaaaa'),
+(2,'ddddddddddd'),
+(3,'gggggggggg');
+
+insert into sestra_svekar (sifra,sestra,svekar)
+values
+(null,1,1),
+(null,2,2),
+(null,3,3);
+
+insert into punac(sifra)
+values
+(null),
+(null),
+(null);
+
+insert into cura(punac)
+values
+(1),
+(2),
+(3);
+
+
+/* SQL
+update cura set gustoca=15.77;
+
+delete from mladic 
+where kuna>15.78;
+
+select kratkamajica from zena
+where hlace like'%ne%';
+
+________________FUNCTION___________________________
+select eks1(kratkamajica,hlace) as eks1 from zena;
+
+DELIMITER $$
+create function eks1(kratkamajica varchar(31), hlace varchar(46)) returns varchar(255)
+begin
+	
+	return (select kratkamajica where hlace like '%ne%');
+
+end;
+$$
+DELIMITER ;
+_____________________________________________________
+
+select a.dukserica , f.asocijalno , e.hlace 
+from svekar a
+inner join sestra_svekar b on a.sifra = b.svekar 
+inner join sestra c on b.sestra = c.sifra 
+inner join zena d on c.sifra = d.sestra 
+inner join muskarac e on d.sifra = e.zena 
+inner join mladic f on e.sifra = f.sifra 
+where d.hlace like'a%' and c.haljina like'%ba%'
+order by e.hlace desc;
+
+
+select a.haljina , a.maraka 
+from sestra a
+left join sestra_svekar b on a.sifra = b.sestra 
+where b.svekar is null ;
+*/
+
---
+++ SQL/Excersises/excersise.6.sql
@@ -0,0 +1,164 @@
+#  c:\xampp\mysql\bin\mysql -uroot < C:\Users\Adam\Desktop\Repositories\PHP-learning\SQL\Excersises\excersise.6.sql
+drop database if exists excersise;
+create database excersise;
+use excersise;
+
+create table prijatelj(
+    sifra int not null primary key auto_increment,
+    haljina varchar(35),
+    prstena int not null,
+    introvertno bit,
+    stilfrizura varchar(36) not null
+);
+
+create table prijatelj_ostavljena (
+    sifra int not null primary key auto_increment,
+    prijatelj int,
+    ostavljena int
+);
+
+create table ostavljena(
+    sifra int not null primary key auto_increment,
+    prviputa datetime not null,
+    kratkamajica varchar(39) not null,
+    drugiputa datetime,
+    maraka decimal(14,10)
+);
+
+create table brat(
+    sifra int not null primary key auto_increment,
+    nausnica int not null,
+    treciputa datetime not null,
+    narukvica int not null,
+    hlace varchar(31),
+    prijatelj int
+);
+
+create table zena(
+    sifra int not null primary key auto_increment,
+    novcica decimal(14,8) not null,
+    narukvica int not null,
+    dukserica varchar (40) not null,
+    haljina varchar (30),
+    hlace varchar(32),
+    brat int
+);
+
+create table decko(
+    sifra int not null primary key auto_increment,
+    prviputa datetime,
+    modelnaocala varchar(41),
+    nausnica int,
+    zena int
+);
+
+create table svekrva(
+    sifra int not null primary key auto_increment,
+    hlace varchar(48) not null,
+    suknja varchar(42) not null,
+    ogrlica int not null,
+    treciputa datetime not null,
+    dukserica varchar(32) not null,
+    narukvica int not null,
+    punac int
+);
+
+create table punac(
+    sifra int not null primary key auto_increment,
+    ekstrovertno bit not null,
+    suknja varchar(30) not null,
+    majica varchar(44) not null,
+    prviputa datetime not null
+);
+
+
+alter table prijatelj_ostavljena add foreign key (prijatelj) references prijatelj (sifra);
+
+alter table prijatelj_ostavljena add foreign key (ostavljena) references ostavljena (sifra);
+
+alter table zena add foreign key (brat) references brat (sifra);
+
+alter table decko add foreign key (zena) references zena (sifra);
+
+alter table svekrva add foreign key (punac) references punac (sifra);
+
+alter table brat add foreign key (prijatelj) references prijatelj (sifra);
+
+insert into prijatelj(sifra,prstena,stilfrizura)
+values
+(null,3,'Tomahawk'),
+(null,1,'Celav'),
+(null,219,'Irokeza')
+;
+
+insert into brat(sifra,nausnica,treciputa,narukvica,prijatelj)
+values 
+(null,3,'2021-10-19 12:34:04',0,1),
+(null,5,'2015-01-24 11:39:10',5,2),
+(null,0,'2019-11-18 10:43:00',0,3)
+;
+
+
+insert into zena(sifra,novcica,narukvica,dukserica,brat)
+values 
+(null,'1589.24',2,'Dukserica je uvijek poderana',1),
+(null,'189.78',0,'Dukserica nije njegova',2),
+(null,'26457.20',7,'Uvijek zaprljana',3)
+;
+
+insert into punac(sifra,ekstrovertno,suknja,majica,prviputa)
+values
+(null,1,'Ne nosi on suknje','Samo tank top','2011-04-12 21:41:13'),
+(null,0,'Ne nosi on suknje','Samo zute','2010-07-12 20:37:02'),
+(null,1,'Ne nosi on suknje','Samo tamne','2001-05-12 01:51:59')
+;
+
+insert into svekrva(sifra,hlace,suknja,ogrlica,treciputa,dukserica,narukvica,punac)
+values
+(null,'Jako kratke hlace','Samo dugacke suknje',18,'2020-07-14 16:45:02','Ne nosi dukserice',2,1),
+(null,'Uske hlace','Samo sarene suknje',1,'2021-04-04 12:35:22','Samo roze dukserice',7,2),
+(null,'Jako kratke hlace s cvijetekima','Ne nosi suknje',8,'1995-08-24 10:55:02','Dugacke sa visokim ovratnikom',14,3)
+;
+
+insert into ostavljena(sifra,prviputa,kratkamajica,drugiputa)
+values
+(null,'2005-11-03 19:45:37','Nosi kad nema nista drugo','2009-01-27 09:27:17'),
+(null,'2007-06-14 14:57:31','Uvijek tamne boje','2009-07-17 14:22:48'),
+(null,'2002-01-05 04:45:54','Kosulja','2009-08-04 07:20:10')
+;
+
+insert into prijatelj_ostavljena(sifra,prijatelj,ostavljena)
+values
+(null,1,2),
+(null,2,1),
+(null,3,3)
+;
+
+insert into decko(sifra,zena)
+values 
+(null,1),
+(null,2),
+(null,3)
+;
+
+update svekrva set suknja = 'Osijek';
+
+delete from decko where modelnaocala < 'ab';
+
+select narukvica from brat
+where treciputa is null ;
+
+select a.drugiputa , g.zena , f.narukvica 
+from ostavljena a
+inner join prijatelj_ostavljena b on a.sifra = b.ostavljena 
+inner join prijatelj c on b.prijatelj = c.sifra 
+inner join brat d on c.sifra = d.prijatelj 
+inner join zena f on d.sifra = f.brat 
+inner join decko g on f.sifra = g.zena 
+where d.treciputa is not null and c.prstena=219
+order by f.narukvica desc;
+
+select a.prstena , a.introvertno 
+from prijatelj a 
+left join prijatelj_ostavljena b on b.prijatelj = a.sifra
+where b.prijatelj is null;
